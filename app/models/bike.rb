adc9a1a713a2807519594587103beee26ab4daab
Miguel Mundi <miguelmundiaragones@gmail.com>
2015-04-01 14:10:31 +0200

    Modified forms

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,7 @@
 class Bike < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image
+	has_attached_file :image, styles: { :medium => "400x400#" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	has_attached_file :image_two
 	has_attached_file :image_three
 	has_attached_file :image_four
